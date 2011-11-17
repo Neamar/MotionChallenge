c294572b0f4eb398e9d7eddcde4e7ba0f0315346
Neamar <neamar@neamar.fr>
2011-11-17 12:59:19 +0000

    Ajout de Prise en main et Acquisition de formes
    git-svn-id: https://shareif.insa-lyon.fr/svn/bm.kinect@1 bc12ac6f-388f-48c2-8376-786384368edd

---
+++ Acquisition de formes/WorkingWithDepthData/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿/////////////////////////////////////////////////////////////////////////
+//
+//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// This code is licensed under the terms of the Microsoft Kinect for
+// Windows SDK (Beta) License Agreement:
+// http://kinectforwindows.org/KinectSDK-ToU
+//
+/////////////////////////////////////////////////////////////////////////
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Coding4Fun.Kinect.Wpf;
+using Microsoft.Research.Kinect.Nui; 
+
+namespace WorkingWithDepthData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Kinect Runtime
+        Runtime nui; 
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            SetupKinect(); 
+
+        }
+
+        private void SetupKinect()
+        {
+            if (Runtime.Kinects.Count == 0)
+            {
+                this.Title = "No Kinect connected";
+            }
+            else
+            {
+                //use first Kinect
+                nui = Runtime.Kinects[0];
+
+                //UseDepthAndPlayerIndex and UseSkeletalTracking
+                nui.Initialize(RuntimeOptions.UseDepthAndPlayerIndex | RuntimeOptions.UseSkeletalTracking);
+
+                //register for event
+                nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
+
+                //DepthAndPlayerIndex ImageType
+                nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240,
+                    ImageType.DepthAndPlayerIndex); 
+            }
+        }
+
+        void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            //Convert depth information for a pixel into color information
+            byte[] ColoredBytes = GenerateColoredBytes(e.ImageFrame); 
+
+            //create an image based on returned colors
+            
+            PlanarImage image = e.ImageFrame.Image;
+            image1.Source = BitmapSource.Create(image.Width, image.Height, 96, 96, PixelFormats.Bgra32, null, 
+                ColoredBytes, image.Width * PixelFormats.Bgra32.BitsPerPixel/ 8); 
+
+
+
+        }
+
+        private byte[] GenerateColoredBytes(ImageFrame imageFrame)
+        {
+            
+            int height = imageFrame.Image.Height;
+            int width = imageFrame.Image.Width;
+
+            //Depth data for each pixel
+            Byte[] depthData = imageFrame.Image.Bits; 
+
+
+            //colorFrame contains color information for all pixels in image
+            //Height x Width x 4 (Red, Green, Blue, empty byte)
+            Byte[] colorFrame = new byte[imageFrame.Image.Height * imageFrame.Image.Width * 4];
+
+            //Bgr32  - Blue, Green, Red, empty byte
+            //Bgra32 - Blue, Green, Red, transparency 
+            //You must set transparency for Bgra as .NET defaults a byte to 0 = fully transparent
+
+            //hardcoded locations to Blue, Green, Red (BGR) index positions       
+            const int BlueIndex = 0;
+            const int GreenIndex = 1;
+            const int RedIndex = 2;
+            const int AlphaIndex = 3;
+                       
+
+            var depthIndex = 0;
+            for (var y = 0; y < height; y++)
+            {
+                
+                var heightOffset = y * width;
+
+                for (var x = 0; x < width; x++)
+                {
+
+                    var index = ((x + 0) + heightOffset) * 4;
+
+                    //var distance = GetDistance(depthData[depthIndex], depthData[depthIndex + 1]);
+                    var distance = GetDistanceWithPlayerIndex(depthData[depthIndex], depthData[depthIndex + 1]);
+
+                    //we are very close
+                    colorFrame[index + BlueIndex] = 0;
+                    colorFrame[index + GreenIndex] = 0;
+                    colorFrame[index + RedIndex] = 0;
+                    colorFrame[index +AlphaIndex] = 0;
+
+                    ////equal coloring for monochromatic histogram
+                    //var intensity = CalculateIntensityFromDepth(distance);
+                    //colorFrame[index + BlueIndex] = intensity;
+                    //colorFrame[index + GreenIndex] = intensity;
+                    //colorFrame[index + RedIndex] = intensity;
+
+                    ////Color a player
+                    if (GetPlayerIndex(depthData[depthIndex]) > 0)
+                    {
+                        colorFrame[index + BlueIndex] = 0;
+                        colorFrame[index + GreenIndex] = 255;
+                        colorFrame[index + RedIndex] = 0;
+                        colorFrame[index + AlphaIndex] = 150;
+                    }
+                    //jump two bytes at a time
+                    depthIndex += 2;
+                }
+            }
+
+            return colorFrame;
+        }
+
+        private static int GetPlayerIndex(byte firstFrame)
+        {
+            //returns 0 = no player, 1 = 1st player, 2 = 2nd player...
+            //bitwise & on firstFrame
+            return (int)firstFrame & 7; 
+        }
+
+
+        private int GetDistanceWithPlayerIndex(byte firstFrame, byte secondFrame)
+        {
+            //offset by 3 in first byte to get value after player index 
+            int distance = (int)(firstFrame >> 3 | secondFrame << 5);
+            return distance;
+        }
+
+        const float MaxDepthDistance = 4000; // max value returned
+        const float MinDepthDistance = 850; // min value returned
+        const float MaxDepthDistanceOffset = MaxDepthDistance - MinDepthDistance;
+
+        public static byte CalculateIntensityFromDepth(int distance)
+        {
+            //formula for calculating monochrome intensity for histogram
+            return (byte)(255 - (255 * Math.Max(distance - MinDepthDistance, 0) 
+                / (MaxDepthDistanceOffset)));
+        }
+
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
+        {
+            //cleanup
+            nui.Uninitialize(); 
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            (image1.Source as BitmapSource).Save("C:\\Temp\\lol.png",ImageFormat.Png);
+        }
+
+        }
+
+    }
+
---
+++ Prise en main/TestKinect/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Research.Kinect.Nui;
+
+namespace TestKinect
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Runtime nui;
+        private void image1_Loaded(object sender, RoutedEventArgs e)
+        {
+            nui = Runtime.Kinects[0];
+            
+            nui.Initialize(RuntimeOptions.UseColor | RuntimeOptions.UseDepth);
+            nui.VideoFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_VideoFrameReady);
+            nui.VideoStream.Open(ImageStreamType.Video, 2, ImageResolution.Resolution640x480, ImageType.Color);
+            
+         //   nui.DepthFrameReady += new EventHandler<ImageFrameReadyEventArgs>(nui_DepthFrameReady);
+         //   nui.DepthStream.Open(ImageStreamType.Depth, 2, ImageResolution.Resolution320x240, ImageType.Depth);
+        }
+
+        void nui_DepthFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            PlanarImage image = e.ImageFrame.Image;
+            image1.Source = BitmapSource.Create(image.Width, image.Height, 96, 96, PixelFormats.Bgr32,
+                null, image.Bits, image.Width * PixelFormats.Bgr32.BitsPerPixel / 8);
+        }
+
+        void nui_VideoFrameReady(object sender, ImageFrameReadyEventArgs e)
+        {
+            PlanarImage image = e.ImageFrame.Image;
+            image1.Source = BitmapSource.Create( image.Width, image.Height, 96, 96, PixelFormats.Bgr32,
+                null, image.Bits, image.Width * image.BytesPerPixel);
+        }
+
+        private void image1_Unloaded(object sender, RoutedEventArgs e)
+        {
+            nui.Uninitialize();
+        }
+    }
+}
