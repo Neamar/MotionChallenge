d5844e61b13b0a8cfecdad9232f9764c9264422b
Stéphane <seng.stephane@gmail.com>
2011-11-17 14:02:58 +0000

    Importation de l'application principale.
    git-svn-id: https://shareif.insa-lyon.fr/svn/bm.kinect@4 bc12ac6f-388f-48c2-8376-786384368edd

---
+++ MotionChallenge/MotionChallenge/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using OpenTK;
+using OpenTK.Graphics.OpenGL;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MotionChallenge
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Composant GLControl
+        OpenTK.GLControl glControl;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Cree le composant GLControl
+            glControl = new OpenTK.GLControl();
+
+            // Ajoute le composant dans le WindowsFormHost
+            windowsFormsHost.Child = glControl;
+            glControl.Paint += new PaintEventHandler(Paint);
+        }
+
+        private void Paint(object sender, PaintEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            glControl.SwapBuffers();
+        }
+    }
+}
