819f3048a0b15c137e5cb71c073edda45a4bae90
Neamar <neamar@neamar.fr>
2011-11-17 14:18:08 +0000

    Refactor WorkingWithDepthData -> MotionCapture
    git-svn-id: https://shareif.insa-lyon.fr/svn/bm.kinect@5 bc12ac6f-388f-48c2-8376-786384368edd

--- Acquisition de formes/MotionCapture/obj/x86/Release/MainWindow.g.cs
+++ Acquisition de formes/MotionCapture/obj/x86/Release/MainWindow.g.cs
@@ -75,7 +75,7 @@ namespace WorkingWithDepthData {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WorkingWithDepthData;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/MotionCapture;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
