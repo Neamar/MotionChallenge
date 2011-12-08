b26ac535cd4bcc5e74408d1ae5f87bd7b2bbc50b
Stéphane <seng.stephane@gmail.com>
2011-12-08 15:31:01 +0000

    Ajout des logs de scores dans le Splash.
    git-svn-id: https://shareif.insa-lyon.fr/svn/bm.kinect@42 bc12ac6f-388f-48c2-8376-786384368edd

--- MotionChallenge/MotionChallenge/Splash.xaml.cs
+++ MotionChallenge/MotionChallenge/Splash.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 
+using Microsoft.VisualBasic;
+
 namespace MotionChallenge
 {
     /// <summary>
@@ -18,6 +21,8 @@ namespace MotionChallenge
     /// </summary>
     public partial class Splash : Window
     {
+        private int gameMode;
+
         public MainWindow mainWindow;
 
         public Splash()
@@ -29,6 +34,11 @@ namespace MotionChallenge
         {
             int playerCount = int.Parse((sender as Button).Name.Replace("nbPlayer", ""));
 
+            // Memorise les informations qui serviront pour le scoring
+            gameMode = playerCount;
+            // Demande le nom des joueurs
+            //Microsoft.VisualBasic.Interaction.InputBox("Noms : ", "Nom des joueurs pour le mode " + (sender as Button).Name, "");
+            
             mainWindow = new MainWindow(playerCount);
             mainWindow.Show();
             mainWindow.Closing += new System.ComponentModel.CancelEventHandler(mainWindow_Closing);
@@ -39,12 +49,30 @@ namespace MotionChallenge
         void mainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             scoreLabel.Content = (sender as MainWindow).scoreLabel.Content;
+
+            // Met a jour le tableau des scores
+            listBox.Items.Add("(" + getModeName() + ") \t" + (sender as MainWindow).scoreLabel.Content);
+
             this.Show();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        string getModeName()
         {
-
+            if (gameMode > 0)
+            {
+                if (gameMode < 3)
+                {
+                    return gameMode + " j";
+                }
+                else
+                {
+                    return "Hard";
+                }
+            }
+            else
+            {
+                return "";
+            }
         }
     }
 }
